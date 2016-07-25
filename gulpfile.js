101f88cfb282d90d2425d0c236add47b06f3df15
Rachel Carden <bamadesigner@gmail.com>
2016-07-24 17:09:53 -0700

    Optimizing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,56 @@
+// Require all the things (that we need)
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var watch = require('gulp-watch');
+var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 
+// Define the source paths for each file type
+var src = {
+    scss: './assets/scss/*.scss',
+    js: ['assets/js/*.js','!assets/js/*.min.js','!assets/js/*-min.js']
+};
+
+// Define the destination paths for each file type
+var dest = {
+	scss: './assets/css',
+	js: './assets/js'
+}
+
+// I've got my eyes on you(r file changes)
+gulp.task('watch', function() {
+	gulp.watch(src.scss, ['sass']);
+	gulp.watch(src.js, ['compress']);
+});
+
+// Sass is pretty awesome, right?
 gulp.task('sass', function() {
-	gulp.src('assets/scss/*.scss')
-		.pipe(watch('assets/scss/*.scss'))
-		.pipe(sass({outputStyle:'compressed'}))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('assets/css'));
-});
-
-gulp.task('compress', function() {
-	gulp.src(['assets/js/*.js','!assets/js/*.min.js','!assets/js/*-min.js'])
-		.pipe(minify({
-			ext: '.min.js'
+	return gulp.src(src.scss)
+		.pipe(sass({
+			outputStyle: 'compressed'
+		})
+		.on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
 		}))
-		.pipe(gulp.dest('assets/js'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(dest.scss));
 });
 
+// Compress all the JS
+gulp.task('compress', function() {
+	return gulp.src(src.js)
+		.pipe(minify({
+			ext: '.min.js',
+			mangle: false
+		}))
+		.pipe(gulp.dest(dest.js))
+});
+
+// Let's get this party started
 gulp.task('default', ['sass','compress'], function() {
-	gulp.watch('assets/scss/*.scss',['sass']);
-	gulp.watch(['assets/js/*.js','!assets/js/*.min.js','!assets/js/*-min.js'],['compress']);
+	gulp.watch(src.scss, ['sass']);
+	gulp.watch(src.js, ['compress']);
 });
